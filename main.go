3f976561e18bd19c4f23702ec0bf7eaf7d2fac30
alfnsnff <alfnsnff@gmail.com>
2024-11-04 15:33:06 +0700

    hotfix: change listen port on dockerfile

--- main.go
+++ main.go
@@ -8,5 +8,5 @@ import (
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	log.Println("listening on port 8080...")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
